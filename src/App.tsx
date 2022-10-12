4a8972ebf5e9e4ef098284dee8039f30a1a0a8aa
Luis Murilo <luiszkm@gmail.com>
2022-10-12 18:18:16 -0300

    up project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+  
+return(
+  <h1 className='text-violet-400'>colleee</h1>
+)
+  
+}
+
---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Checkbox,CheckboxProps  } from "./Checkbox";
+
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  args: { },
+  argsTypes: {},
+  decorators: [
+    (Story) =>{
+      return (
+        <div>
+          {Story()}
+        </div>
+      )
+    }
+  ]
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {}
