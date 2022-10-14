ee29675ba96ddf706cba164fdca831ad339de234
Luis Murilo <luiszkm@gmail.com>
2022-10-13 22:04:17 -0300

    add auto test

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,8 +16,13 @@ module.exports = {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true,
+
   },
+  "staticDirs":[
+    "../public"
+  ],
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/DesignerSystem_IgniteLab/'
